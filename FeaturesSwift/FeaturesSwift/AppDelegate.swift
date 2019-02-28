a84ff2e321146c879106cfee6dd13ebb5183bb04
Sitora Guliamova <sitora_guliamova@epam.com>
2019-02-28 14:12:39 +0300

    fixed error with pbxproj in master

---
+++ FeaturesSwift/FeaturesSwift/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  FeaturesSwift
+//
+//  Created by Sitora Guliamova on 2/28/19.
+//  Copyright © 2019 Sitora Guliamova. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
